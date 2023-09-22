c3dd6df25b8a7273a24d07df26f729a2a3a2b4b5
Hans Erik Jepsen <haeje@jfmedier.dk>
2023-09-22 11:24:17 +0200

    Removed e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,12 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
-const config: PlaywrightTestConfig[] = [
-	{
-		testDir: './tests/lib',
-		testMatch: /(.+\.)?(test)\.[jt]s/
-	},
-	{
-		webServer: {
-			command: 'npm run build && npm run preview',
-			port: 4173
-		},
-		testDir: './tests/e2e',
-		testMatch: /(.+\.)?(test)\.[jt]s/
-	}
-];
+const config: PlaywrightTestConfig = {
+	// webServer: {
+	// 	command: 'npm run build && npm run preview',
+	// 	port: 4173,
+	// },
+	testDir: './tests',
+	testMatch: /(.+\.)?(test)\.[jt]s/,
+};
 
 export default config;
